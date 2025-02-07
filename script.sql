71237ecbaa6816abeabe39091d28f56209592540
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2025-02-07 07:25:25 -0300

    generic

--- script.sql
+++ script.sql
@@ -1,5 +1,2 @@
--- mysqldump -p -u adriano deliverynode > deliverynode.sql; 
--- pwd: 453231
-
 -- https://lefred.be/content/mysql-8-0-and-wrong-dates/
 -- set @@SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
